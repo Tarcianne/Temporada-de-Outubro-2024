fe9bf598ea5a4599c41b6ef0decde7c60bb581c1
Tarcianne S.Rocha <tarctata26@gmail.com>
2024-10-08 13:41:59 -0300

    Add files via upload

---
+++ digite0.py
@@ -0,0 +1,7 @@
+while True:
+    numero=int(input("Digite um número (use 0 para encerrar):"))
+    if numero==0:
+        print("Execução encerrada com sucesso!")
+        break
+    else:
+        print(f"Você digitou:{numero}")
